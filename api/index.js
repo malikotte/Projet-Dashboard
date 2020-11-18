d79977348c3ad26dfd7bafed3c3fa259240ae8f2
malikotte <malikderkaoui@gmail.com>
2020-11-18 10:50:03 +0100

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,48 +1,47 @@
 const express = require('express');
-const faker = require('faker'); // Simuler données 
+
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const app = express();
 var cors = require('cors');
-app.listen(3001, () => console.log('Listening on port 3001'));
+const path = require('path');
+const dbName = "api";
+const dbUrl = `mongodb://localhost:27017/${dbName}`;
+
+
+
+
+// Connexion à la DB
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true
+},
+    () => console.log('Connexion effectuée !'));
+
+
+// Port
+
+const port = process.env.PORT || 3001;
+
+// Initi cors Middleware
 app.use(cors());
 
-// List Users
-const users = [];
-// Créer la version de notre api 
-const versionApi = '/api/vl';
+// Initialize Body Parser Middleware
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// GET /api/v1/users
+// Initialize Public directory
+/*app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})*/
 
-app.get(`${versionApi}/users`, (req, res) => {
-    res.json({
-        data: users
-    })
+app.get('/', (req, res) => {
+    res.send('<h1>wes wes</h1>')
 });
 
-// GET /api/v1/users/:id
+const postRoutes = require('./routes/posts');
+app.use('/posts', postRoutes);
 
-app.get(`${versionApi}/users/:id`, (req, res) => {
-    const id = req.params.id - 1;
-
-    res.json({
-        data: users[id] || null
-    })
-})
-// POST /api/v1/users
-app.post(`${versionApi}/users`, (req, res) => {
-    const data = req.body;
-    users.push(data);
-
-    console.log(data);
-    res.json({
-        index: users.length,
-        data: users[users.length - 1]
-    })
-})
-
-// PUT /api/v1/users/:id
-
-// DELETE /api/v1/users/:id
+app.listen(port, () => {
+    console.log('Serveur started on port ', port)
+});
