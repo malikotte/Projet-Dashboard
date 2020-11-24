2d6c6e25e418b0a95f6e235842ddfeea4d604547
malikotte <malikderkaoui@gmail.com>
2020-11-24 19:13:18 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,18 @@ import "./Header.css";
 import logo from './covid.png';
 import { Link } from "react-router-dom";
 
+import styled, { keyframes } from 'styled-components';
+import { zoomIn } from 'react-animations';
+import { Typewriter } from 'react-typewriting-effect'
+import 'react-typewriting-effect/dist/index.css'
+
+
+const bounceAnimation = keyframes`${zoomIn}`;
+
+const BouncyDiv = styled.div`
+  animation: 1s ${bounceAnimation};
+`;
+
 class Header extends Component {
     render() {
         return (
@@ -18,17 +30,18 @@ class Header extends Component {
 
                 </nav>
                 <div className="alert alert-warning" role="alert">
-
-                    <img src={logo} alt="covid" style={{ width: "20px", height: "20px", marginRight: "5px" }} />
+                    <BouncyDiv>
+                        <img src={logo} alt="covid" style={{ width: "20px", height: "20px", marginRight: "5px" }} />
              Ensemble, respectons les gestes barrières
              <img src={logo} alt="covid" style={{ width: "20px", height: "20px", marginLeft: "5px" }} />
+                    </BouncyDiv>
                 </div>
                 <div className="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
 
                     <h2 className="display-4">COVID-19</h2>
                     <p className="lead">
-                        Application sur la pandémie du COVID-19
-        </p>
+                        <Typewriter string='Application sur la pandémie du COVID-19' delay={80} stopBlinkinOnComplete />
+                    </p>
 
                 </div>
             </div>
@@ -37,4 +50,5 @@ class Header extends Component {
 }
 
 export default Header;
+
 
