fe20ea6bc5c86034226c8f39f66ca7ec03bf96dd
malikotte <malikderkaoui@gmail.com>
2020-11-24 17:41:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,48 +7,23 @@ import Admin from './components/Admin/Admin.js';
 import Form from './components/Form/Form.js';
 import React, { Component } from 'react';
 
-import logo from './covid.png';
 import "./components/Header/Header.css";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <nav className="navbar navbar-dark bg-dark">
-            <Link to="/" className="text-white">
-              COVID Analyzer
-                </Link>
-            <span class="navbar-text">
-              <Link to="/Admin" className="Header-a">Administration</Link>
-            </span>
-
-          </nav>
-          {/*<Header />*/}
-          <div class="alert alert-warning" role="alert">
-
-            <img src={logo} alt="covid" style={{ width: "20px", height: "20px", marginRight: "5px" }} />
-             Ensemble, respectons les gestes barrières
-             <img src={logo} alt="covid" style={{ width: "20px", height: "20px", marginLeft: "5px" }} />
-          </div>
-          <div className="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-
-            <h2 className="display-4">COVID-19</h2>
-            <p className="lead">
-              Application sur la pandémie du COVID-19
-        </p>
-
-          </div>
+          <Header />
           <Switch>
             <Route path="/Admin">
               <div className="container mt-3">
                 <div className="row mb-5">
-                  <div className="col-sm-6">
+                  <div className="col-xs-12 col-md-6">
                     <Admin />
                   </div>
-                  <div className="col-sm-6">
+                  <div className="col-md-6">
                     <Form />
                   </div>
                 </div>
