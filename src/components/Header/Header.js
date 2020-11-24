102804c37851d26f87dc08fc70c2895c96a52b08
malikotte <malikderkaoui@gmail.com>
2020-11-24 17:38:35 +0100

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import "./Header.css";
+import logo from './covid.png';
+import { Link } from "react-router-dom";
 
 class Header extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark">
+            <div>
+                <nav className="navbar navbar-dark bg-dark">
+                    <Link to="/" className="text-white">
+                        COVID Analyzer
+                </Link>
+                    <span class="navbar-text">
+                        <Link to="/Admin" className="Header-a">Administration</Link>
+                    </span>
 
-                <span className="text-white">
 
-                    COVID Analyze
-                </span>
-                <span class="navbar-text">
-                    <a href="./Admin.html" className="Header-a">Administration</a>
-                </span>
-            </nav>
+                </nav>
+                <div className="alert alert-warning" role="alert">
+
+                    <img src={logo} alt="covid" style={{ width: "20px", height: "20px", marginRight: "5px" }} />
+             Ensemble, respectons les gestes barrières
+             <img src={logo} alt="covid" style={{ width: "20px", height: "20px", marginLeft: "5px" }} />
+                </div>
+                <div className="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+
+                    <h2 className="display-4">COVID-19</h2>
+                    <p className="lead">
+                        Application sur la pandémie du COVID-19
+        </p>
+
+                </div>
+            </div>
         );
     }
 }
 
 export default Header;
+
