bc19ee2ab6931de5d377dd32be15006aa236dde6
malikotte <malikderkaoui@gmail.com>
2020-11-24 15:25:07 +0100

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -48,7 +48,7 @@ class Form extends Component {
 
     handleSubmit = (event) => {
         //https://express-app-covid.herokuapp.com/posts/update/${this.state.id_user}
-        fetch(`http://localhost:3001/posts/update/${this.state.id_user}`, {
+        fetch(`https://express-app-covid.herokuapp.com/posts/update/${this.state.id_user}`, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -69,7 +69,7 @@ class Form extends Component {
     componentDidUpdate(prevProps) {
         if (this.state.firstname !== prevProps.firstname) {
             // https://express-app-covid.herokuapp.com/posts/
-            fetch('http://localhost:3001/posts/')
+            fetch('https://express-app-covid.herokuapp.com/posts/')
                 .then((response) => {
                     return response.json()
                 })
@@ -86,7 +86,7 @@ class Form extends Component {
 
     delete(id) {
         //https://express-app-covid.herokuapp.com/posts/
-        fetch('http://localhost:3001/posts/' + id, {
+        fetch('https://express-app-covid.herokuapp.com/posts/' + id, {
             method: 'DELETE',
 
             headers: {
