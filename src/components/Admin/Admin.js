55bd651180fd7f8d9864e08ba8372a0b3c6b227f
Malik <malikderkaoui@gmail.com>
2020-12-14 00:03:14 +0100

    Update Admin.js

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -37,8 +37,8 @@ class Admin extends Component {
         event.preventDefault();
     }
     handleSubmit = (event) => {
-        // https://express-app-covid.herokuapp.com/posts/add
-        fetch('http://localhost:3001/posts/add', {
+        // https://express-app-covid.herokuapp.com/posts/add  ou http://localhost:3001/posts/add
+        fetch('https://express-app-covid.herokuapp.com/posts/add', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
